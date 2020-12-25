6b53e938a33f05eda055fdf959db20719b342f3c
Yash Garudkar <yashgarudkar@gmail.com>
2020-12-25 14:36:55 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import QRcode from 'react-qr-code';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    txt: '',
+  };
+
+  onChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      txt: e.target.value,
+    });
+    console.log(this.state.txt);
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h2>QR-Code Generator</h2>
+        <div className='container'>
+          <div className='input-box'>
+            <input
+              type='text'
+              onChange={(e) => this.onChange(e)}
+              value={this.state.txt}
+              placeholder='Start typing...'
+            />
+          </div>
+          <div className='qr'>
+            <QRcode value={this.state.txt} />
+          </div>
+        </div>
+        <footer>
+          <h5>
+            Made with ❤️ by:{' '}
+            <a href='http://www.github.com/yashgkar' rel="noreferrer" target='_blank'>
+              Yash G.
+            </a>
+          </h5>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
